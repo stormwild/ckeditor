78fe6aa2af972884762cb8b1c302b605fa89a1e4
Alexander R Torrijos <artorrijos@gmail.com>
2022-12-15 16:08:41 +0800

    Custom build working with TodoList

---
+++ typings.d.ts
@@ -0,0 +1,163 @@
+//credit goes to https://github.com/ckeditor/ckeditor5/issues
+declare module '@ckeditor/ckeditor5-react' {
+  const CKEditor: any
+  export default CKEditor
+}
+
+declare module '@ckeditor/ckeditor5-editor-classic/src/classiceditor' {
+  const ClassicEditor: any
+  export default ClassicEditor
+}
+
+declare module '@ckeditor/ckeditor5-essentials/src/essentials' {
+  const Essentials: any
+  export default Essentials
+}
+
+declare module '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter' {
+  const UploadAdapter: any
+  export default UploadAdapter
+}
+
+declare module '@ckeditor/ckeditor5-autoformat/src/autoformat' {
+  const Autoformat: any
+  export default Autoformat
+}
+
+declare module '@ckeditor/ckeditor5-basic-styles/src/bold' {
+  const Bold: any
+  export default Bold
+}
+
+declare module '@ckeditor/ckeditor5-basic-styles/src/italic' {
+  const Italic: any
+  export default Italic
+}
+
+declare module '@ckeditor/ckeditor5-block-quote/src/blockquote' {
+  const BlockQuote: any
+  export default BlockQuote
+}
+
+declare module '@ckeditor/ckeditor5-ckbox/src/ckbox' {
+  const CKBox: any
+  export default CKBox
+}
+
+declare module '@ckeditor/ckeditor5-ckfinder/src/ckfinder' {
+  const CKFinder: any
+  export default CKFinder
+}
+
+declare module '@ckeditor/ckeditor5-easy-image/src/easyimage' {
+  const EasyImage: any
+  export default EasyImage
+}
+
+declare module '@ckeditor/ckeditor5-heading/src/heading' {
+  const Heading: any
+  export default Heading
+}
+
+declare module '@ckeditor/ckeditor5-image/src/image' {
+  const Image: any
+  const PictureEditing: any
+  export default Image
+}
+
+declare module '@ckeditor/ckeditor5-image/src/imagecaption' {
+  const ImageCaption: any
+  export default ImageCaption
+}
+
+declare module '@ckeditor/ckeditor5-image/src/imagestyle' {
+  const ImageStyle: any
+  export default ImageStyle
+}
+
+declare module '@ckeditor/ckeditor5-image/src/imagetoolbar' {
+  const ImageToolbar: any
+  export default ImageToolbar
+}
+
+declare module '@ckeditor/ckeditor5-image/src/imageupload' {
+  const ImageUpload: any
+  export default ImageUpload
+}
+
+declare module '@ckeditor/ckeditor5-indent/src/indent' {
+  const Indent: any
+  export default Indent
+}
+
+declare module '@ckeditor/ckeditor5-link/src/link' {
+  const Link: any
+  export default Link
+}
+
+declare module '@ckeditor/ckeditor5-list/src/list' {
+  const List: any
+  export default List
+}
+
+declare module '@ckeditor/ckeditor5-list/src/todolist' {
+  const TodoList: any
+  export default TodoList
+}
+
+declare module '@ckeditor/ckeditor5-media-embed/src/mediaembed' {
+  const MediaEmbed: any
+  export default MediaEmbed
+}
+
+declare module '@ckeditor/ckeditor5-paragraph/src/paragraph' {
+  const Paragraph: any
+  export default Paragraph
+}
+
+declare module '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice' {
+  const PasteFromOffice: any
+  export default PasteFromOffice
+}
+
+declare module '@ckeditor/ckeditor5-image/src/pictureediting' {
+  const PictureEditing: any
+  export default PictureEditing
+}
+
+declare module '@ckeditor/ckeditor5-table/src/table' {
+  const Table: any
+  export default Table
+}
+
+declare module '@ckeditor/ckeditor5-table/src/tabletoolbar' {
+  const TableToolbar: any
+  export default TableToolbar
+}
+
+declare module '@ckeditor/ckeditor5-typing/src/texttransformation' {
+  const TextTransformation: any
+  export default TextTransformation
+}
+
+declare module '@ckeditor/ckeditor5-cloud-services/src/cloudservices' {
+  const CloudServices: any
+  export default CloudServices
+}
+
+declare module '@ckeditor/ckeditor5-dev-utils' {
+  const bundler: any
+  const styles: any
+  export { bundler, styles }
+}
+
+declare module '@ckeditor/ckeditor5-dev-webpack-plugin' {
+  const CKEditorWebpackPlugin: any
+  export default CKEditorWebpackPlugin
+}
+
+// declare module '@stormwild/ckeditor' {
+//   const CustomEditor: ClassicEditor
+
+//   export default CustomEditor
+// }
