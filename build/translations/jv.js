c37142c18e584ac8dc36b2657be2dec171b54bcc
Alexander R Torrijos <artorrijos@gmail.com>
2022-12-15 19:38:22 +0800

    Added build

---
+++ build/translations/jv.js
@@ -0,0 +1,1 @@
+!function(i){const n=i.jv=i.jv||{};n.dictionary=Object.assign(n.dictionary||{},{"%0 of %1":"%0 saking %1",Bold:"Kandhel","Bulleted List":"",Cancel:"Batal",Italic:"Miring","Numbered List":"",Save:"Rimat","Show more items":"Tampilaken langkung kathah","To-do List":""}),n.getPluralForm=function(i){return 0}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
